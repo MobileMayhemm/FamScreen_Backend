0c99aa6e8767990f68a6a1a617ebcc4d19b23295
Aziz <aziz@Azizs-MacBook-Air.local>
2024-11-07 09:19:58 +0700

    dummy gambar

--- database/migrations/2024_11_06_062512_create_films_table.php
+++ database/migrations/2024_11_06_062512_create_films_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('film', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('poster')->nullable();
+            $table->string('poster')->default('https://i.etsystatic.com/29363253/r/il/a53f80/3399179376/il_570xN.3399179376_sgv5.jpg');
             $table->string('judul');
             $table->text('deskripsi');
             $table->year('tahun_rilis');
